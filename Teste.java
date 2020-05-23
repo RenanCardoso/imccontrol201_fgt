0278c55d5368455bf2c3a11405fad4b3bfdd5887
repl.it user <replituser@example.com>
2020-05-23 00:21:08 +0000

    commitando alteracoes da ultima aula

--- Teste.java
+++ Teste.java
@@ -6,10 +6,22 @@ class Teste {
 		Aluno b = new Aluno("Belmiro Silva");
 		
     
+    Avaliacao a1 = new Avaliacao(50,2);
+
+    //double indice =  IMC.calcular(90,2);
+    //String classificacao = IMC.situacao(indice);
+
+    double indice =  a1.indice();
+    String classificacao = a1.classificacao();
+
 
     //Relatorio saida
     System.out.println("Listagem de alunos:");
     System.out.println(a.getNome());
+
+    System.out.println(indice);
+    System.out.println(classificacao);
+
     System.out.println(b.getNome());
 
     //
